a1978fbe0bd3a695b4cead7f7fe3066a47cac82a
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-06-13 11:09:59 -0700

    Player Class Init

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public int maxRounds;
+    private int rounds;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private static Player instance;
+    
+    public float maxHealth;
+    private float health;
+    public float lives;
+    private bool alive;
+    
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void TakeDamage(float damage)
+    {
+
+    }
+	
+    public bool IsAlive()
+    {
+        return alive;
+    }
+
+    public static Player getInstance()
+    {
+        return instance;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Update is called once per frame
+	void Update () {
+		
+	}
+}
