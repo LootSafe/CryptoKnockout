22a42fac5fbd3803ff620acb3cc40452bd564fc0
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-06-14 01:18:09 -0400

    CameraRumbler
    Rumbles the camera when called. Has public debug variable can test using the W key.
    
    Camera Rumbles, if it's called within a period of time it intensifies to stress the amount of damage done in such a short period of time.

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class CameraRumbler : MonoBehaviour
+{
+    enum SHAKESTATE { SHAKING, NOTSHAKING };
+
+    SHAKESTATE shakeState;
+    Vector3 originalPosition;
+
+    float timeShaking;
+    float shakeDuration;
+    float shakeIntensity;
+    int intensityMultiplier;
+
+    public bool debugging = false;
+
+    /* Methods */
+
+    void Start()
+    {
+        shakeState = SHAKESTATE.NOTSHAKING;
+
+        /* Configurables */
+
+        shakeDuration = 0.06f;
+        shakeIntensity = 0.5f;
+        timeShaking = 0.0f;
+        intensityMultiplier = 1;
+    }
+
+    void Update()
+    {
+        /* Shake */
+
+        if (timeShaking > 0)
+        {
+            Camera.main.transform.localPosition = originalPosition + Random.insideUnitSphere * shakeDuration * (shakeIntensity * intensityMultiplier);
+
+            timeShaking -= Time.deltaTime * 0.1f;
+        }
+        else if (timeShaking <= 0 && shakeState == SHAKESTATE.SHAKING)
+        {
+            shakeState = SHAKESTATE.NOTSHAKING;
+            intensityMultiplier = 1;
+        }
+
+        /* Debug */
+
+        if (debugging)
+        {
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                standardAttack();
+            }
+        }
+    }
+
+    void standardAttack()
+    {
+        if (shakeState == SHAKESTATE.SHAKING)
+        {
+            intensityMultiplier++;
+            Camera.main.transform.localPosition = originalPosition;
+        }
+        else
+        {
+            originalPosition = Camera.main.transform.localPosition;
+        }
+
+        shakeState = SHAKESTATE.SHAKING;
+        timeShaking = shakeDuration;
+    }
+}
