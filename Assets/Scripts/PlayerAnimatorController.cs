3644fd22dfede24ad30909732cbdbcb4592be4d2
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-06-15 18:46:18 -0400

    Perliniminary Animation Logic

---
+++ Assets/Scripts/PlayerAnimatorController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour {
+
+	enum ANIMATION_STATE { GROUNDED, NOTGROUNDED};
+    enum CURRENT_ANIMATION { IDLE, JUMP, HURT, DEAD, HIGHPUNCH, LOWPUNCH, HIGHKICK, LOWKICK, BLOCK, SPECIALATTACKONE};
+
+    // Animation logic will be handled here
+}
