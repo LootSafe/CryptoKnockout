9387b5c3f8df9b1193ef8f335740a2e761ef282e
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-06-22 20:53:51 -0400

    More Parallax
    More contained. Need to do the Y-Axis stuff but may leave it for now until the controls are set on.

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,57 +4,103 @@ using UnityEngine;
 public enum MOVESTATE { LEFT, RIGHT, CENTERAL };
 
 public class Parallax : MonoBehaviour
-{
-    public List<GameObject> parralaxSprites;
-    public float baseIncrement = 0.01f;
-    public float speed = 0.01f;
+{    
+    public float speed;
+    public float incrementBetweenSprites = 0.01f;
 
-    float increment;
-    MOVESTATE movestate;
+    List<GameObject> pSprites;
+    List<Vector3> pSpritesOrgPos;
+    float xCurrentPos, yCurrentPos, lastPosX, lastPosY;
 
-    void Update () {
-        
-        /* State Logic */
+    void Awake()
+    {
+        GameObject daddyLax = GameObject.Find("Parrallax");
 
-        if (Input.GetKey(KeyCode.A))
+        pSprites = new List<GameObject>();
+        pSpritesOrgPos = new List<Vector3>();
+
+        foreach (Transform childLax in daddyLax.transform)
         {
-            movestate = MOVESTATE.LEFT;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            movestate = MOVESTATE.RIGHT;
-        }
-        else
-        {
-            movestate = MOVESTATE.CENTERAL;
+            pSprites.Add(childLax.gameObject);
+            pSpritesOrgPos.Add(childLax.gameObject.transform.position);
         }
 
-        /* Move Logic */
-
-        if (movestate == MOVESTATE.LEFT)
-        {
-            increment = baseIncrement;
-
-            for (int i = 0; i <= parralaxSprites.Count - 1; i++)
-            {
-                increment += i * (speed / 10);
-
-                Vector3 current = parralaxSprites[i].transform.position;
-                parralaxSprites[i].transform.position = new Vector3(current.x += increment, current.y, current.z);
-            }
-        }
-        else if(movestate == MOVESTATE.RIGHT)
-        {
-            increment = baseIncrement;
-
-            for (int i = 0; i <= parralaxSprites.Count - 1; i++)
-            {
-                increment += i * (speed / 10); 
-
-                Vector3 current = parralaxSprites[i].transform.position;
-                parralaxSprites[i].transform.position = new Vector3(current.x -= increment, current.y, current.z);
-            }
-        }
-
+        lastPosX = transform.position.x;
+        lastPosY = transform.position.y;
     }
+
+    void Update ()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float yMovement = transform.position.y;
+
+        xCurrentPos = transform.position.x;
+        yCurrentPos = transform.position.y;
+
+        if(HasPositionChanged())
+        {
+            for (int i = 0; i <= pSprites.Count - 1; i++)
+            {
+                float xIncrement = 0;
+                float yIncrement = 0;
+                
+                Vector3 current = pSprites[i].transform.position;
+
+                if (lastPosX != xCurrentPos)
+                {
+                    if (xMovement < 0)
+                    {
+                        xIncrement += (incrementBetweenSprites * speed) * (i + 1) / 10;
+                    }
+                    else
+                    {
+                        xIncrement -= (incrementBetweenSprites * speed) * (i + 1) / 10;
+                    }
+
+                    current.x += xIncrement;
+                }
+                       
+                /*
+                if (lastPosY != yCurrentPos)
+                {
+                   if (yMovement > 0)
+                   {
+                        yIncrement += (incrementBetweenSprites * speed) * (i + 1) / 100;
+
+                        if (pSpritesOrgPos[i].y <= (current.y += yIncrement))
+                        {
+                            current.y += yIncrement;
+                        }
+                   }
+                   
+                    else
+                    {
+                        yIncrement -= (incrementBetweenSprites * speed) * (i + 1) / 100;
+
+                        if (pSpritesOrgPos[i].y <= (current.y -= yIncrement))
+                        {
+                            current.y += yIncrement;
+                        }
+                    }
+                }
+                */
+
+                pSprites[i].transform.position = new Vector3(current.x, current.y, current.z);
+            }
+        }        
+
+        lastPosX = transform.position.x;
+        lastPosY = transform.position.y;
+    }
+
+    bool HasPositionChanged()
+    {
+        if (lastPosX != xCurrentPos)
+            return true;
+        if (lastPosY != yCurrentPos)
+            return true;
+
+        return false;
+    }
+
 }
