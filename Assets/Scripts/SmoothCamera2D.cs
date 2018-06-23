e922303a3867d84e78ec87a403a81ddc893b15dc
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-06-23 13:34:58 -0400

    2DSmoothCamera Script
    Will edit and hook it up later.

---
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SmoothCamera2D : MonoBehaviour
+{
+    public float dampTime = 0.15f;
+    public float offSetY = 0.2f;
+
+    const float center = 0.5f;
+
+    Vector3 velocity = Vector3.zero;
+    Camera cam;
+    GameObject target = null;
+
+    void Start()
+    {
+        cam = Camera.main;    
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+
+        }
+        else
+        {
+            Vector3 point = cam.WorldToViewportPoint(transform.position);
+            Vector3 delta = transform.position - cam.ViewportToWorldPoint(new Vector3(center, center + offSetY, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+    }
+}
