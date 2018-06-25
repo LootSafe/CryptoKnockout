21c15da7f24d6ba669d729e338abf5a7ceedffc0
SoftDevAndy <sweeneyandrew.softdev@gmail.com>
2018-06-25 14:19:43 -0400

    Animation Controller Stuff

--- Assets/Scripts/PlayerAnimatorController.cs
+++ Assets/Scripts/PlayerAnimatorController.cs
@@ -2,8 +2,66 @@
 
 public class PlayerAnimatorController : MonoBehaviour {
 
-	enum ANIMATION_STATE { GROUNDED, NOTGROUNDED};
-    enum CURRENT_ANIMATION { IDLE, JUMP, HURT, DEAD, HIGHPUNCH, LOWPUNCH, HIGHKICK, LOWKICK, BLOCK, SPECIALATTACKONE};
+	public enum GROUNDED_STATE { GROUNDED, NOTGROUNDED};
+    public enum ANIMATION_STATE { IDLE, BLOCK, DEAD, JUMP, HURT, LOWPUNCH, LOWKICK, HIGHPUNCH, HIGHKICK, SPECIALATTACKONE };
 
     // Animation logic will be handled here
+
+    GROUNDED_STATE groundedState;
+    ANIMATION_STATE currentAnimationState;
+    
+    /* Animation State */
+
+    public ANIMATION_STATE GetCurrentAnimationState()
+    {
+        return currentAnimationState;
+    }
+
+    public bool SetAnimationState(ANIMATION_STATE animationState)
+    {
+        switch(animationState)
+        {
+            case ANIMATION_STATE.IDLE:
+                return true;
+            case ANIMATION_STATE.BLOCK:
+                return true;
+            case ANIMATION_STATE.DEAD:
+                return true;
+            case ANIMATION_STATE.JUMP:
+                return true;
+            case ANIMATION_STATE.HURT:
+                return true;
+            case ANIMATION_STATE.LOWPUNCH:
+                return true;
+            case ANIMATION_STATE.LOWKICK:
+                return true;
+            case ANIMATION_STATE.HIGHPUNCH:
+                return true;
+            case ANIMATION_STATE.HIGHKICK:
+                return true;
+            case ANIMATION_STATE.SPECIALATTACKONE:
+                return true;
+        }
+
+        return false;
+    }
+
+    /* State Helpers */
+
+    public void SetGroundedState(bool isGrounded)
+    {
+        if (isGrounded)
+        {
+            groundedState = GROUNDED_STATE.GROUNDED;
+        }
+        else
+        {
+            groundedState = GROUNDED_STATE.NOTGROUNDED;
+        }
+    }
+
+    public GROUNDED_STATE GetGroundedState(bool isGrounded)
+    {
+        return groundedState;
+    }
 }
