c52667c662131af1aea6281a11e7bb702f78ef57
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-08 23:29:54 -0700

    Add Functionality to Buttons

---
+++ Assets/Scripts/MainMenu/ButtonHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BtnLocalPlay()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    public void BtnMultiplayerPlay()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    public void BtnQuit()
+    {
+        Application.Quit();
+    }
+}
