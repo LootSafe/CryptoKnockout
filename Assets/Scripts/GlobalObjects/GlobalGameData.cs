ce41decc9b530bf93b1cf840dbda6d707f4d2afd
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-09 00:17:02 -0700

    Implement Global Object Structure

---
+++ Assets/Scripts/GlobalObjects/GlobalGameData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalGameData : MonoBehaviour {
+
+    public Game.GameMode selectedGameMode = Game.GameMode.LOCALMULTIPLAYER;
+    public string selectedLevel = "Denver";
+    public string player1Char = "Bjorn";
+    public string player2Char = "Doge";
+
+
+    private static bool created = false;
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/MainMenu/ButtonHandler.cs
+++ Assets/Scripts/MainMenu/ButtonHandler.cs
@@ -17,6 +17,7 @@ public class ButtonHandler : MonoBehaviour {
 
     public void BtnLocalPlay()
     {
+        
         SceneManager.LoadScene("CharacterSelect");
     }
 
