d0de983bd04491d33b3fa625eccd251392158359
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-09 02:03:04 -0700

    Network Temp Fix

--- Assets/Scripts/NetworkHandler.cs
+++ Assets/Scripts/NetworkHandler.cs
@@ -6,11 +6,23 @@ using UnityEngine.Networking;
 public class NetworkHandler : NetworkManager
 {
     private Vector3 playerSpawnPos;
+    private Game game;
+    
+    void Awake()
+    {
+        game = Game.GetInstance();
+    }
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
+        if (!game) return;
+        if(Game.GetInstance().GetGameMode() == Game.GameMode.LOCALMULTIPLAYER)
+        {
+            return;
+        }
         var player = (GameObject)GameObject.Instantiate(playerPrefab, playerSpawnPos, Quaternion.identity);
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        Logger.Instance.Message(DEVELOPER.ADAM, "Player is being spawned");
     }
 
 }
