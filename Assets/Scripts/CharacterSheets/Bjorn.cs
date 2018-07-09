1244ef18918719dad03f215c07ac17c17f0f4cc6
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-09 03:20:42 -0700

    Add Bjorn and Doge Character Sheets

---
+++ Assets/Scripts/CharacterSheets/Bjorn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bjorn : Character {
+
+    public Bjorn()
+    {
+        this.health = 1000;
+        this.strength = 10;
+        this.defence = 10;
+        this.moveSpeed = 0.1f;
+        this.name = "Bjorn";
+    }
+
+    public override float CalculateDamage(float damage)
+    {
+        return damage;
+    }
+
+}
---
+++ Assets/Scripts/CharacterSheets/Doge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doge : Character {
+
+    public Doge()
+    {
+        this.health = 1000;
+        this.strength = 10;
+        this.defence = 10;
+        this.moveSpeed = 0.1f;
+        this.name = "Doge";
+    }
+
+    public override float CalculateDamage(float damage)
+    {
+        return damage;
+    }
+}
