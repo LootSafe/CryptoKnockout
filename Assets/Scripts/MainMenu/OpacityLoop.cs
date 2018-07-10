8ec450196951f8fafd3663b45a1d5da0ce84d4df
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-10 11:30:41 -0700

    Intro Screen Changes

---
+++ Assets/Scripts/MainMenu/OpacityLoop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OpacityLoop : MonoBehaviour {
+
+    public float speed = 0.05f;
+    float direction = 1;
+    public float maxAlpha = 1f;
+    public float minAlpha = 0.2f;
+    public MaskableGraphic graphic;
+    float lastAlpha = 1;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!graphic) return;
+        Debug.Log("Alpha: " + graphic.color.a);
+        if (lastAlpha > maxAlpha || lastAlpha < minAlpha) direction*= -1;
+        float newAlpha = lastAlpha + speed * direction;
+        lastAlpha = graphic.color.a;
+        graphic.color = new Color(graphic.color.r, graphic.color.g, graphic.color.b, newAlpha);
+
+        
+    }
+}
