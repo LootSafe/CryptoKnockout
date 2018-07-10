9f093791201a276740282c65c0e3d8c9b9a0c129
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-10 12:16:58 -0700

    Fix Opacity Loop

--- Assets/Scripts/MainMenu/OpacityLoop.cs
+++ Assets/Scripts/MainMenu/OpacityLoop.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 public class OpacityLoop : MonoBehaviour {
 
-    public float speed = 0.05f;
+    public float speed = 0.0001f;
     float direction = 1;
     public float maxAlpha = 1f;
     public float minAlpha = 0.2f;
+    public float delay = 0.066f;
     public MaskableGraphic graphic;
-    float lastAlpha = 1;
+
+    float lastUpdate;
+
+    void Start()
+    {
+        lastUpdate = Time.time;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (!graphic) return;
-        Debug.Log("Alpha: " + graphic.color.a);
-        if (lastAlpha > maxAlpha || lastAlpha < minAlpha) direction*= -1;
-        float newAlpha = lastAlpha + speed * direction;
-        lastAlpha = graphic.color.a;
-        graphic.color = new Color(graphic.color.r, graphic.color.g, graphic.color.b, newAlpha);
-
-        
+        if (Time.time - lastUpdate > delay)
+        {
+            float newAlpha = graphic.color.a + speed * direction;
+            graphic.color = new Color(graphic.color.r, graphic.color.g, graphic.color.b, newAlpha);
+            lastUpdate = Time.time;
+            if (graphic.color.a >= maxAlpha || graphic.color.a <= minAlpha) direction *= -1;
+        }
     }
 }
