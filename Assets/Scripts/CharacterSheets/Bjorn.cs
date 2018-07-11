f83dad41593ce87548f699bd3cf5349edf517833
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-10 22:40:05 -0700

    Update sheets with abstract classes

--- Assets/Scripts/CharacterSheets/Bjorn.cs
+++ Assets/Scripts/CharacterSheets/Bjorn.cs
@@ -23,4 +23,29 @@ public class Bjorn : Character {
         AnimatorOverrideController p = (AnimatorOverrideController)Resources.Load("Animator/Characters/Bjorn", typeof(AnimatorOverrideController));
         return p;
     }
+
+    public override void moveKick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void movePunch()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void special1()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void special2()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ultra()
+    {
+        throw new System.NotImplementedException();
+    }
 }
