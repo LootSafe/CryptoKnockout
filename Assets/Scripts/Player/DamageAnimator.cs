9ea7b01594d448a687c90a646ae448691d50c3a6
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-15 15:10:12 -0700

    Add Damage Indicator(Red Flash) and Update player Prefab

---
+++ Assets/Scripts/Player/DamageAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageAnimator : MonoBehaviour {
+    float lastHit;
+    public float animateSpeed = 10;
+    public float hueIntensity = .4f;
+    public float duration = 0.2f;
+    Color orgColor;
+	// Use this for initialization
+	void Start () {
+        orgColor = GetComponent<SpriteRenderer>().color;
+	}
+
+    public void TriggerSmallHit()
+    {
+        lastHit = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float saturation = ((Time.time - lastHit) / duration);
+        Debug.Log("Saturation:" + saturation);
+        GetComponent<SpriteRenderer>().color = new Color(1, saturation, saturation);
+	}
+}
--- Assets/Scripts/System/Player.cs
+++ Assets/Scripts/System/Player.cs
@@ -71,6 +71,8 @@ public class Player : NetworkBehaviour {
             }
         }
 
+        GetComponent<DamageAnimator>().TriggerSmallHit();
+
     }
 	
     public bool IsAlive()
