900c30d76812603765b5bd1b1c23877050f7a107
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-15 13:41:08 -0700

    Time Update Scripts Updates

---
+++ Assets/Scripts/HUD/TimerUpdate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUpdate : MonoBehaviour {
+    public Text display;
+    Game game;
+	// Use this for initialization
+	void Start () {
+        //display.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!game)
+        {
+            game = Game.GetInstance();
+            return;
+        }
+
+            int secondsLeft = (int)game.GetRemainingRoundTime();
+            if(secondsLeft < 1)
+            {
+                secondsLeft = 0;
+            }
+            display.text = secondsLeft + "";
+        
+	}
+}
