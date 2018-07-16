96f5d30b5ca1162a3d916121508dbc27dbaa9a4a
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-15 23:46:20 -0700

    Update Death Trap With Custom Script

---
+++ Assets/Scripts/HitSystem/DeathTrapScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrapScript : MonoBehaviour {
+
+    public Player player;
+    float lastHit;
+    public float delay = .02f;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void TriggerEnable()
+    {
+        lastHit = Time.time;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && other != player)
+        {
+            float damageDealt = other.GetComponent<Player>().TakeDamage(10, player);
+            if (player) player.AddToScore(damageDealt);
+        }
+    }
+}
