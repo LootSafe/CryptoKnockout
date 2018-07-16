10657b0d15828f9a95d4c47829c9fdf51c6a344c
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-16 00:21:36 -0700

    Update Laser to only work during Fighting

--- Assets/Scripts/LevelComponents/LaserRandomScript.cs
+++ Assets/Scripts/LevelComponents/LaserRandomScript.cs
@@ -9,14 +9,21 @@ public class LaserRandomScript : MonoBehaviour {
     public float fireTime =  1f;
     private float lastFire;
     private bool active;
+    Game game;
 	// Use this for initialization
 	void Start () {
         laserSprite.SetActive(false);
         lastFire = Time.time;
+        game = Game.GetInstance();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!game)
+        {
+            game = Game.GetInstance();
+            return;
+        }
         if (active)
         {
             if (Time.time - lastFire >= fireTime + Random.Range(0.1f, 0.3f))
@@ -28,8 +35,10 @@ public class LaserRandomScript : MonoBehaviour {
         }
         else
         {
+            if (game.GetState() != Game.State.FIGHTING) return;
             if (Time.time - lastFire >= delay + Random.Range(0.1f, 10f))
             {
+                
                 lastFire = Time.time;
                 laserSprite.SetActive(true);
                 active = true;
