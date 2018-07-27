b2010e180bcfcb786cdb305313329d7f1c3cfbe2
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-07-27 00:15:17 -0700

    Splash Updates to the EXTREEEEMEEEE

---
+++ Assets/Revolver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Revolver : MonoBehaviour
+{
+
+    private float RotateSpeed = 5f;
+    private float Radius = 20f;
+
+    private Vector2 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+        _centre = GetComponent<RectTransform>().position;
+    }
+
+    private void Update()
+    {
+        Radius = GetComponentInParent<RectTransform>().sizeDelta.x + 5;
+        _angle += RotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+        GetComponent<RectTransform>().position = _centre + offset;
+    }
+
+
+
+}
