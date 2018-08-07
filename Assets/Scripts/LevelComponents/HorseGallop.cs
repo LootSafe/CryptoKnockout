32f79a55f7bd22ffdc6b62eeee8953e9c0f85823
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-08-07 02:00:52 -0700

    Version Update + Android Fixes + Denver Updates

--- Assets/Scripts/LevelComponents/HorseGallop.cs
+++ Assets/Scripts/LevelComponents/HorseGallop.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class HorseGallop : MonoBehaviour {
 
     GameObject[] markers;
-    Vector2 currentTarget;
+    public Vector2 currentTarget;
     bool arrived = false;
     public float arrivedDistance = 2;
     public float speed = 0.2f;
+    public Transform sprite;
+    public float pauseFrequency = 0.2f;
+    public float pauseTimeMin = 0.1f;
+    public float pauseTimeMax = 2f;
+    public float pauseWaitMin = 2f;
+    public float pauseWaitMax = 5f;
+
+    private bool paused;
+
+    private float pauseTimer;
+
 	// Use this for initialization
 	void Start () {
         markers = GameObject.FindGameObjectsWithTag("BackgroundMarkers");
@@ -18,6 +29,52 @@ public class HorseGallop : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        UpdatePauseSequence();
+        UpdatePosition();
+	}
+
+    void UpdatePauseSequence()
+    {
+        if (paused)
+        {
+            if(Time.time >= pauseTimer)
+            {
+                paused = false;
+                if (Random.Range(0, 1) > 0.5f)
+                {
+                    CycleTarget();
+                }
+            }
+            
+        } else
+        {
+
+            if (Time.time >= pauseTimer + Random.Range(pauseWaitMin, pauseWaitMax))
+            {
+                if(Random.Range(0,2) <= pauseFrequency)
+                {
+                    Pause();
+                }
+            }
+        }
+    }
+
+    void UpdatePosition()
+    {
+        if (paused) return;
+
+        int direction = 0;
+        if (currentTarget.x > transform.position.x)
+        {
+            direction = 1;
+                sprite.localScale = new Vector3(-Mathf.Abs(sprite.localScale.x), sprite.localScale.y, sprite.localScale.z);
+            
+        }
+        else
+        {
+            direction = -1;
+            sprite.localScale = new Vector3(Mathf.Abs(sprite.localScale.x), sprite.localScale.y, sprite.localScale.z);
+        }
         //If arrived
         if (!arrived && Mathf.Abs(transform.position.x - currentTarget.x) <= arrivedDistance)
         {
@@ -27,17 +84,33 @@ public class HorseGallop : MonoBehaviour {
         // Location Markers --> 5
         if (currentTarget == null || arrived)
         {
-            float targetX = markers[Random.Range(0, markers.Length)].transform.position.x;
-            float targetY = markers[Random.Range(0, markers.Length)].transform.position.y;
-            currentTarget = new Vector2(targetX, targetY);
+            CycleTarget();
             arrived = false;
-        } else
-        {
-            transform.position = new Vector2(transform.position.x + speed, transform.position.y);
         }
+        else
+        {
+            Debug.Log("Moving");
 
+            transform.position = new Vector2(transform.position.x + (speed * direction), transform.position.y);
+        }
+    }
 
+    public void Pause()
+    {
+        paused = true;
+        pauseTimer = Time.time + Random.Range(pauseTimeMin, pauseTimeMax);
+    }
 
+    public void Resume()
+    {
+        paused = false;
+        pauseTimer = 0f;
+    }
 
-	}
+    public void CycleTarget()
+    {
+        float targetX = markers[Random.Range(0, markers.Length)].transform.position.x;
+        float targetY = markers[Random.Range(0, markers.Length)].transform.position.y;
+        currentTarget = new Vector2(targetX, targetY);
+    }
 }
