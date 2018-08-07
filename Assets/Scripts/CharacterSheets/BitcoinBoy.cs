9f60dc004ea5497a79e89dc70fa6b7850d2dae4e
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-08-07 14:04:16 -0700

    Init Character Sheets for BTCBoy and EthBot

---
+++ Assets/Scripts/CharacterSheets/BitcoinBoy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitcoinBoy : Character {
+
+    public BitcoinBoy()
+    {
+        this.health = 90;
+        this.strength = 10;
+        this.defence = 10;
+        this.moveSpeed = 10;
+        this.name = "Doge";
+    }
+
+    public override float CalculateDamage(float damage)
+    {
+        return damage;
+    }
+
+    public override AnimatorOverrideController GetAnimationController()
+    {
+        AnimatorOverrideController p = (AnimatorOverrideController)Resources.Load("Animator/Characters/Doge", typeof(AnimatorOverrideController));
+        
+        return p;
+    }
+
+    public override void MoveKick()
+    {
+        player.foot.GetComponent<PlayerDamage>().TriggerEnable();
+    }
+
+    public override void MoveBlock()
+    {
+        
+    }
+    public override void MovePunch()
+    {
+        player.fist.GetComponent<PlayerDamage>().TriggerEnable();
+    }
+
+    public override void MoveSpecial1()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveSpecial2()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveUltra()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/CharacterSheets/EthBot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthBot : Character {
+
+    public EthBot()
+    {
+        this.health = 90;
+        this.strength = 10;
+        this.defence = 10;
+        this.moveSpeed = 10;
+        this.name = "EthBot";
+    }
+
+    public override float CalculateDamage(float damage)
+    {
+        return damage;
+    }
+
+    public override AnimatorOverrideController GetAnimationController()
+    {
+        AnimatorOverrideController p = (AnimatorOverrideController)Resources.Load("Animator/Characters/Doge", typeof(AnimatorOverrideController));
+        
+        return p;
+    }
+
+    public override void MoveKick()
+    {
+        player.foot.GetComponent<PlayerDamage>().TriggerEnable();
+    }
+
+    public override void MoveBlock()
+    {
+        
+    }
+    public override void MovePunch()
+    {
+        player.fist.GetComponent<PlayerDamage>().TriggerEnable();
+    }
+
+    public override void MoveSpecial1()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveSpecial2()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveUltra()
+    {
+        throw new System.NotImplementedException();
+    }
+}
