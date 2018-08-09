8aecc5f1b63131f61d06773c17d495539a5d73e8
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-08-08 20:11:53 -0700

    Update Targetting Radius

--- Assets/Scripts/LevelComponents/LaserRandomScript.cs
+++ Assets/Scripts/LevelComponents/LaserRandomScript.cs
@@ -7,8 +7,12 @@ public class LaserRandomScript : MonoBehaviour {
     public GameObject laserSprite;
     public float delay = 1f;
     public float fireTime =  1f;
+    public float targetRange = 20f;
+    public Transform LaserSource;
     private float lastFire;
     private bool active;
+    private Player[] players;
+    private Transform currentTarget;
     Game game;
 	// Use this for initialization
 	void Start () {
@@ -19,11 +23,44 @@ public class LaserRandomScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //Check for game started
         if (!game)
         {
             game = Game.GetInstance();
             return;
         }
+
+        //Get Players From Game
+        UpdatePlayers();
+
+        //Fire
+        TriggerRandomFire();
+
+	}
+
+    void CheckForTargets()
+    {
+        List<Transform> viableTargets = new List<Transform>();
+        //Check for Radius
+        foreach (Player p in players)
+        {
+            if (Vector2.Distance(LaserSource.position, p.transform.position) <= targetRange)
+            {
+                viableTargets.Add(p.transform);
+            }
+        }
+        if (viableTargets.Count >= 0)
+        {
+            currentTarget = viableTargets[Random.Range((int)0, (int)viableTargets.Count)];
+        } else
+        {
+            currentTarget = null;
+        }
+
+    }
+
+    void TriggerRandomFire()
+    {
         if (active)
         {
             if (Time.time - lastFire >= fireTime + Random.Range(0.1f, 0.3f))
@@ -38,16 +75,30 @@ public class LaserRandomScript : MonoBehaviour {
             if (game.GetState() != Game.State.FIGHTING) return;
             if (Time.time - lastFire >= delay + Random.Range(0.1f, 10f))
             {
-                
+
                 lastFire = Time.time;
                 laserSprite.SetActive(true);
                 active = true;
             }
         }
-	}
+    }
 
-    void getTarget()
+
+    //TODO needs to be optimized
+    void UpdatePlayers()
     {
-
+        if(players == null || players.Length == 0)
+        {
+            players = game.GetPlayers();
+            return;
+        }
+       foreach(Player p in players)
+        {
+            if (!p)
+            {
+                players = game.GetPlayers();
+                return;
+            }
+        }
     }
 }
