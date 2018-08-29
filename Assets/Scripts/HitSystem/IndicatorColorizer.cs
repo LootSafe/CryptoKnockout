f1631047c36730692c2afb6a76d5b75bc1b5c5f4
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-08-29 01:32:27 -0700

    Implement Colorizers for Crit Markers

---
+++ Assets/Scripts/HitSystem/IndicatorColorizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndicatorColorizer : MonoBehaviour {
+
+
+    public Color[] bgColors;
+    public Color[] fgColors;
+    public Color[] txtColors;
+
+    public Image bg;
+    public Image fg;
+    public Text txt;
+        
+    // Use this for initialization
+	void Start () {
+
+        bg.color = bgColors[Random.Range(0, bgColors.Length)];
+        fg.color = fgColors[Random.Range(0, fgColors.Length)];
+        txt.color = txtColors[Random.Range(0, txtColors.Length)];
+	}
+	
+
+}
