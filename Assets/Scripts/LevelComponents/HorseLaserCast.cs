c441dd9eafc7664fd7664918dad4aea5ab7b26d4
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-01 01:11:36 -0700

    Update horse Boy

--- Assets/Scripts/LevelComponents/HorseLaserCast.cs
+++ Assets/Scripts/LevelComponents/HorseLaserCast.cs
@@ -8,16 +8,24 @@ public class HorseLaserCast : MonoBehaviour {
     public Vector2 randomOffsetMin;
     public Vector2 randomOffsetMax;
 
+    public LineRenderer line;
+    public Transform damageSource;
+
     public bool debug;
 
+    void Start()
+    {
+        
+    }
 	void Update () {
+        damageSource.gameObject.SetActive(false);
         Vector2 offsets = new Vector2(Random.Range(randomOffsetMin.x, randomOffsetMax.x), 
                                       Random.Range(randomOffsetMin.y, randomOffsetMax.y));
 
         Vector2 direction = (Vector2)laser.GetCurrentTarget().position + offsets;
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, laser.GetCurrentTarget().position , 50.0f);
-
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction , 50.0f);
+        damageSource.gameObject.SetActive(true);
         if (debug)
         {
             Debug.DrawRay(transform.position, direction * 100, Color.green);
@@ -27,12 +35,17 @@ public class HorseLaserCast : MonoBehaviour {
         if (hit.collider != null)
         {
             Transform objectHit = hit.transform;
-            Debug.Log(objectHit.parent.name);
+            if(debug) Debug.Log("I Hit" + objectHit.parent.name);
+            UpdateLaserDrawing(objectHit.position);
+            damageSource.position = objectHit.position;
+            
         }
     }
 
-    void UpdateLaserDrawing()
+    void UpdateLaserDrawing(Vector2 end)
     {
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, (Vector3)end);
 
     }
 }
