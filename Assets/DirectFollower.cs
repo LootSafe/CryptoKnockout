f407c3a767dbf47edf13c7cfdeec921215fa1968
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-11 13:35:05 -0700

    UI Particle Updates

---
+++ Assets/DirectFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectFollower : MonoBehaviour {
+    public RectTransform followObject;
+    private RectTransform rect;
+	// Use this for initialization
+	void Start () {
+        rect = GetComponent<RectTransform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rect.position = followObject.position;
+	}
+}
