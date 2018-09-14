a7baaa7b37a414d63cdd678d981e126208fe2588
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-14 03:01:33 -0700

    Active Input Manager In Scenes

---
+++ Assets/PauseMenuButtonSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseMenuButtonSelector : MonoBehaviour {
+    public EventSystem e;
+    public Button b;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+    void OnEnable()
+    {
+        e.SetSelectedGameObject(b.gameObject);    
+    }
+
+
+
+// Update is called once per frame
+void Update () {
+	    	
+	}
+}
