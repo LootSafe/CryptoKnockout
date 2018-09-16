659fb484e3c0b137adbd243172955b1d29438dd7
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-16 05:54:23 -0700

    Character Select Screen Update

--- Assets/CharacterSelector.cs
+++ Assets/CharacterSelector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class CharacterSelector : MonoBehaviour {
 
 
@@ -10,10 +11,14 @@ public class CharacterSelector : MonoBehaviour {
     public EventSystem player2E;
     public GameObject[] selected = new GameObject[9];
     public GameObject[] starts = new GameObject[9];
+    public bool[] ready = new bool[9];
     private InputLocker locker;
+
+    GlobalGameData data;
 
     private static CharacterSelector instance;
     public static CharacterSelector GetInstance() { return instance; }
+    public GameObject startToContinueText;
 
 	// Use this for initialization
 	void Start () { 
@@ -23,22 +28,48 @@ public class CharacterSelector : MonoBehaviour {
         selected[2].GetComponent<CharacterSelectButtons>().Select();
         locker = new InputLocker();
         instance = this;
+        data = GlobalGameData.GetInstance();
     }
 	
 	// Update is called once per frame
 	void Update () {
         CheckForInput(PlayerIndex.One);
         CheckForInput(PlayerIndex.Two);
+        UpdateReady();
+    }
+
+    void UpdateReady()
+    {
+        if(ready[1] && ready[2])
+        {
+            startToContinueText.SetActive(true);
+
+            if (GamePad.GetButton(CButton.B))
+            {
+                ready[1] = false;
+                ready[2] = false;
+            }
+
+            if (GamePad.GetButton(CButton.A) || GamePad.GetButton(CButton.Start))
+            {
+                NextScene();
+            }
+
+
+        }
+        else
+        {
+            startToContinueText.SetActive(false);
+        }
     }
 
     void CheckForInput(PlayerIndex pi)
     {
-        //Horizontal
         GameObject s = selected[(int)pi];
         if (!s) s = starts[(int)pi];
         CharacterSelectButtons b = s.GetComponent<CharacterSelectButtons>();
         if (!b) return;
-        if (!b.interactable) ;
+        if (!b.interactable) s = starts[(int)pi];
 
         Control<CAxis> cHor = new Control<CAxis>(CAxis.LX, pi);
         Control<CAxis> cVer = new Control<CAxis>(CAxis.LY, pi);
@@ -47,62 +78,93 @@ public class CharacterSelector : MonoBehaviour {
         float horizontal = GamePad.GetAxis(cHor);
         float vertical = GamePad.GetAxis(cVer);
 
-        //HORIZONTAL
-        if(horizontal != 0)
+        if (!ready[(int)pi])
         {
-            if(!Locked(cHor))
+            //HORIZONTAL
+            if (horizontal != 0)
             {
-                if (horizontal < 0)
+                if (!Locked(cHor))
                 {
-                    GameObject swap = s.GetComponent<CharacterSelectButtons>().left;
-                    if (swap && swap.GetComponent<CharacterSelectButtons>()) s = swap;
-                    Lock(cHor);
-                }
-                else if (horizontal > 0)
-                {
-                    GameObject swap = s.GetComponent<CharacterSelectButtons>().right;
-                    if (swap && swap.GetComponent<CharacterSelectButtons>()) s = swap;
-                    Lock(cHor);
+                    if (horizontal < 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().left;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cHor);
+                    }
+                    else if (horizontal > 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().right;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cHor);
+                    }
                 }
             }
-        } 
-        else
-        {
-            Unlock(cHor);
-        }
-
-        //VERTICAL
-        if (vertical != 0)
-        {
-            if(!Locked(cVer))
+            else
             {
-                if (vertical < 0)
+                Unlock(cHor);
+            }
+
+            //VERTICAL
+            if (vertical != 0)
+            {
+                if (!Locked(cVer))
                 {
-                    GameObject swap = s.GetComponent<CharacterSelectButtons>().down;
-                    if (swap && swap.GetComponent<CharacterSelectButtons>()) s = swap;
-                    Lock(cVer);
-                }
-                else if (vertical > 0)
-                {
-                    GameObject swap = s.GetComponent<CharacterSelectButtons>().up;
-                    if (swap && swap.GetComponent<CharacterSelectButtons>()) s = swap;
-                    Locked(cVer);
+                    if (vertical < 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().down;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cVer);
+                    }
+                    else if (vertical > 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().up;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Locked(cVer);
+                    }
                 }
             }
+            else
+            {
+                Unlock(cVer);
+            }
+
+
+
+
+
+            if (selected[(int)pi] != s)
+            {
+                selected[(int)pi].GetComponent<CharacterSelectButtons>().Deselect();
+                s.GetComponent<CharacterSelectButtons>().Select();
+            }
+            selected[(int)pi] = s;
+
+            if (GamePad.GetButton(CButton.A, pi))
+            {
+                if (pi == PlayerIndex.One)
+                {
+                    data.player1Char = selected[(int)pi].GetComponent<CharacterSelectButtons>().character;
+                    ready[(int)pi] = true;
+                }
+                else if (pi == PlayerIndex.Two)
+                {
+                    data.player2Char = selected[(int)pi].GetComponent<CharacterSelectButtons>().character;
+                    ready[(int)pi] = true;
+                }
+
+            }
+
         }
         else
         {
-            Unlock(cVer);
+            if (GamePad.GetButton(CButton.B, pi))
+            {
+                ready[(int)pi] = false;
+            }
         }
 
 
-
-        if (selected[(int)pi] != s)
-        {
-            selected[(int)pi].GetComponent<CharacterSelectButtons>().Deselect();
-            s.GetComponent<CharacterSelectButtons>().Select();
-        }
-        selected[(int)pi] = s;
+       
     }
 
     public bool Locked(Control<CAxis> control)
@@ -150,5 +212,11 @@ public class CharacterSelector : MonoBehaviour {
             if (control.pi == PlayerIndex.Any) return;
             axisLocks[(int)control.pi, (int)control.control] = true;
         }
+    }
+
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("MapSelect");
     }
 }
--- Assets/Scripts/CharacterSelect/AvatarUpdator.cs
+++ Assets/Scripts/CharacterSelect/AvatarUpdator.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class AvatarUpdator : MonoBehaviour {
 
     public CharacterSelector selections;
+    public GameObject ReadyIndicator;
     public int playerNumber;
     public Text text;
+
+    public Color readyColor;
 
     GameObject p1Selection, p2Selection;
 	// Use this for initialization
@@ -17,8 +20,8 @@ public class AvatarUpdator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        p1Selection = selections.GetSelected()[0];
-        p2Selection = selections.GetSelected()[1];
+        p1Selection = selections.GetSelected()[1];
+        p2Selection = selections.GetSelected()[2];
 
         
 
@@ -29,7 +32,19 @@ public class AvatarUpdator : MonoBehaviour {
                 Sprite sprite = p1Selection.GetComponent<CharacterSelectButtons>().avatar_left;
                 GetComponent<Image>().sprite = sprite;
                 text.text = p1Selection.GetComponent<CharacterSelectButtons>().character.ToString();
+                text.color = Color.white;
 
+            }
+
+            if (selections.ready[1])
+            {
+                ReadyIndicator.SetActive(false);
+                text.text = "READY";
+                text.color = readyColor;
+            }
+            else
+            {
+                ReadyIndicator.SetActive(true);
             }
         }
         else if(playerNumber == 2)
@@ -40,6 +55,18 @@ public class AvatarUpdator : MonoBehaviour {
                 Sprite sprite = p2Selection.GetComponent<CharacterSelectButtons>().avatar_right;
                 GetComponent<Image>().sprite = sprite;
                 text.text = p2Selection.GetComponent<CharacterSelectButtons>().character.ToString();
+                text.color = Color.white;
+            }
+
+            if (selections.ready[2])
+            {
+                ReadyIndicator.SetActive(false);
+                text.text = "READY";
+                text.color = readyColor;
+            }
+            else
+            {
+                ReadyIndicator.SetActive(true);
             }
         }
 
