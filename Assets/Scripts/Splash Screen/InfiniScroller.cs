8e66589eaaaf8dba3bedbbc839db21367bbf857c
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-19 00:56:55 -0700

    Update Infinis Scroller With Material Preservation

--- Assets/Scripts/Splash Screen/InfiniScroller.cs
+++ Assets/Scripts/Splash Screen/InfiniScroller.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class InfiniScroller : MonoBehaviour {
     Image image;
     public float speed = 0.5f;
+    private Vector2 OriginalOffset;
     // Use this for initialization
     void Start () {
         image = GetComponent<Image>();
+        OriginalOffset = image.material.mainTextureOffset;
 	}
 	
 	// Update is called once per frame
@@ -17,4 +19,9 @@ public class InfiniScroller : MonoBehaviour {
         Vector2 offset = new Vector2(Time.time * speed, 0);
         image.material.mainTextureOffset = offset;
 	}
+
+    void OnDisable()
+    {
+        image.material.mainTextureOffset = OriginalOffset;
+    }
 }
