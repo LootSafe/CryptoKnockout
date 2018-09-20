08af90dc5fb045a9e43d55fe74a2b21c3bb41fb4
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-20 01:40:51 -0700

    Audio System updates and FootSteps

---
+++ Assets/FootSteps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour {
+
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        AudioSystem.Register(audioSource);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
