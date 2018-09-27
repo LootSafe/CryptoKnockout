6ef4df7eb502c3529ebab36d6b6c08d346684bdf
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-27 04:28:43 -0700

    Ethbot Super Updates

--- Assets/Scripts/Player/SuperAnimators/EthbotSuper.cs
+++ Assets/Scripts/Player/SuperAnimators/EthbotSuper.cs
@@ -3,22 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EthbotSuper : SuperAnimationControl {
+    private int yDirection, xDirection;
+    public float runSpeed = 0.4f;
+    public GameObject opponentHit;
+
+    public AudioSource audioSource;
+    public AudioClip chargingAudio;
+    public AudioClip explosionAudio;
+    public AudioClip hahaAudio;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+
+    public override void StartSequence()
+    {
+        animationObject.SetActive(false);
+        base.StartSequence();
+
+        if (opponent.transform.position.y < transform.position.y)
+        {
+            yDirection = -1;
+        }
+        else
+        {
+            yDirection = 1;
+        }
+
+        if (opponent.transform.position.x < transform.position.x)
+        {
+            xDirection = -1;
+        }
+        else
+        {
+            xDirection = 1;
+        }
+        AudioSystem.Play(audioSource, chargingAudio);
+        base.StartSequence();
+    }
 
     public override void UpdateIntro()
     {
-        base.UpdateIntro();
+        if (Mathf.Abs(transform.position.x - opponent.transform.position.x) > opponent.GetComponent<BoxCollider2D>().size.x / 3)
+        {
+            Debug.Log(runSpeed * xDirection);
+            rigidbody.AddForce(new Vector2(runSpeed * xDirection, 0));
+        }
+        else
+        {
+            AudioSystem.Play(audioSource, explosionAudio);
+            rigidbody.velocity = Vector2.zero;
+            NextSequence();
+        }
     }
     public override void UpdateMid()
     {
-        base.UpdateMid();
+
+        if (Time.time >= postTime)
+        {
+            AudioSystem.Play(audioSource, explosionAudio, true);
+            opponentHit.transform.position = opponent.transform.position + new Vector3(0, 0.6f, 0);
+            animationObject.SetActive(true);
+            NextSequence();
+        }
     }
     public override void UpdatePost()
     {
-        base.UpdatePost();
+        if (Time.time >= endTime)
+        {
+            animationObject.SetActive(false);
+            player.NotifySuperComplete();
+            NextSequence();
+        }
     }
 
     public override void UpdateEnd()
     {
+        AudioSystem.Play(audioSource, hahaAudio);
         base.UpdateEnd();
     }
 }
