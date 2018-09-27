a9790a38cc6514922842f9ebfb9a0d312bd97334
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-27 06:02:20 -0700

    MenuButton Wiggle

---
+++ Assets/MenuButtonExtension.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuButtonExtension : MonoBehaviour, ISelectHandler, IPointerEnterHandler {
+
+    public Oscillator oscillator;
+    public EventSystem e;
+    Button b;
+    Vector2 originalPosition;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        oscillator.enabled = true;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        oscillator.enabled = false;
+    }
+
+    public void OnMouseOver()
+    {
+        Debug.Log("Button Selected");
+    }
+
+    public void OnMouseEnter()
+    {
+        
+    }
+
+    public void OnMouseExit()
+    {
+        oscillator.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        oscillator = GetComponent<Oscillator>();b = GetComponent<Button>();
+        originalPosition = b.GetComponent<RectTransform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(e.currentSelectedGameObject == gameObject)
+        {
+            oscillator.enabled = true;
+        }
+        else
+        {
+            oscillator.enabled = false;
+            GetComponent<RectTransform>().position = originalPosition;
+        }
+	}
+}
