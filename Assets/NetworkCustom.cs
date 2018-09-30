324b2b8337519920fc4fd01e786d84bd5b3e7435
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-30 02:38:21 -0700

    Revert "Player prefab ref fixes"
    This reverts commit 613bb49f3f5d5dca33a425fcd2754e1b78ac851b.

--- Assets/NetworkCustom.cs
+++ Assets/NetworkCustom.cs
@@ -36,8 +36,8 @@ public class NetworkCustom : NetworkManager
 
         }
 
-        player.GetComponent<Player>().InitializeWithCharacter(Character.Get(selectedCharacter));
-        player.GetComponent<Player>().respawn();
+        player.GetComponentInChildren<Player>().InitializeWithCharacter(Character.Get(selectedCharacter));
+        player.GetComponentInChildren<Player>().respawn();
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
     }
 
