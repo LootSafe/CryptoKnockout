481b2e09a796a0a18fc01f0beeac07419fabf68f
Adam Sanchez <adam.sanchez@adamsanchez.net>
2018-09-30 11:28:17 -0700

    CharacterSelection Networked

---
+++ Assets/Scripts/CharacterSelect/AvatarUpdatorMP.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarUpdatorMP : MonoBehaviour {
+
+    public CharacterSelector selections;
+    public GameObject ReadyIndicator;
+    public int playerNumber;
+    public Text text;
+    public Sprite Abutton;
+    public Sprite KeyButton1;
+    public Sprite KeyButton2;
+
+    public Color readyColor;
+
+    GameObject p1Selection, p2Selection;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        p1Selection = selections.GetSelected()[1];
+        p2Selection = selections.GetSelected()[2];
+        //Debug.Log(GamePad.GetMapping(PlayerIndex.One).Controller.name);
+        //Debug.Log(GamePad.GetMapping(PlayerIndex.Two).Controller.name);
+        if (playerNumber == 1)
+        {
+            if (p1Selection && p1Selection.GetComponent<CharacterSelectButtons>())
+            {
+                Sprite sprite = p1Selection.GetComponent<CharacterSelectButtons>().avatar_left;
+                GetComponent<Image>().sprite = sprite;
+                text.text = p1Selection.GetComponent<CharacterSelectButtons>().character.ToString();
+                text.color = Color.white;
+
+                if (GamePad.GetMapping(PlayerIndex.One).Controller.name == "Keyboard")
+                {
+                    ReadyIndicator.GetComponent<Image>().sprite = KeyButton1;
+                }
+                else if (GamePad.GetMapping(PlayerIndex.One).Controller.name == "Keyboard1")
+                {
+                    ReadyIndicator.GetComponent<Image>().sprite = KeyButton2;
+                }
+                else
+                {
+                    ReadyIndicator.GetComponent<Image>().sprite = Abutton;
+                }
+            }
+
+            if (selections.ready[1])
+            {
+                ReadyIndicator.SetActive(false);
+                text.text = "READY";
+                text.color = readyColor;
+
+            }
+            else
+            {
+                ReadyIndicator.SetActive(true);
+            }
+        }
+        else if(playerNumber == 2)
+        {
+            if (p2Selection && p2Selection.GetComponent<CharacterSelectButtons>())
+            {
+
+                Sprite sprite = p2Selection.GetComponent<CharacterSelectButtons>().avatar_right;
+                GetComponent<Image>().sprite = sprite;
+                text.text = p2Selection.GetComponent<CharacterSelectButtons>().character.ToString();
+                text.color = Color.white;
+
+                if (GamePad.GetMapping(PlayerIndex.Two).Controller.name.ToLower().Equals("keyboard"))
+                {
+                    ReadyIndicator.GetComponent<Image>().sprite = KeyButton1;
+                }
+                else if (GamePad.GetMapping(PlayerIndex.Two).Controller.name.ToLower().Equals("keyboard1"))
+                {
+                    ReadyIndicator.GetComponent<Image>().sprite = KeyButton2;
+                }
+                else
+                {
+                    ReadyIndicator.GetComponent<Image>().sprite = Abutton;
+                }
+            }
+
+            if (selections.ready[2])
+            {
+                ReadyIndicator.SetActive(false);
+                text.text = "READY";
+                text.color = readyColor;
+
+            }
+            else
+            {
+                ReadyIndicator.SetActive(true);
+            }
+        }
+
+	}
+}
---
+++ Assets/Scripts/CharacterSelect/CharacterSelectorMP.cs
@@ -0,0 +1,250 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class CharacterSelectorMP : NetworkBehaviour {
+
+
+    public EventSystem player1E;
+    public GameObject[] selected = new GameObject[9];
+    public GameObject[] starts = new GameObject[9];
+    public bool[] ready = new bool[9];
+
+    //NW
+    [SyncVar]
+    float countDown;
+
+
+    private float startDelayTimer;
+
+
+    GlobalGameData data;
+
+    private static CharacterSelectorMP instance;
+    public static CharacterSelectorMP GetInstance() { return instance; }
+    public GameObject startToContinueText;
+
+    private InputLocker locker;
+
+	// Use this for initialization
+	void Start () {
+        startDelayTimer = Time.time;
+        selected[1] = starts[1];
+        selected[1].GetComponent<CharacterSelectButtons>().Select();
+        selected[2] = starts[2];
+        selected[2].GetComponent<CharacterSelectButtons>().Select();
+        instance = this;
+        data = GlobalGameData.GetInstance();
+        locker = new InputLocker();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!(Time.time >= startDelayTimer + 0.5)) return;
+        UpdateEscape();
+        CheckForInput(PlayerIndex.One);
+        UpdateReady();
+    }
+
+    void UpdateEscape()
+    {
+        
+        if (GamePad.GetState(PlayerIndex.One).Released(CButton.B) && !ready[1])
+        {
+            Back();
+        }
+            
+    }
+    void UpdateReady()
+    {
+        
+        if (ready[1] && ready[2])
+        {
+            startToContinueText.SetActive(true);
+
+            if (GamePad.GetState().Released(CButton.B))
+            {
+                ready[1] = false;
+                ready[2] = false;
+            }
+
+
+            if (GamePad.GetState().Released(CButton.A) || GamePad.GetState().Released(CButton.Start))
+            {
+                    NextScene();
+            }
+            else
+            {
+            }
+
+
+        }
+        else
+        {
+            startToContinueText.SetActive(false);
+        }
+    }
+
+    void CheckForInput(PlayerIndex pi)
+    {
+
+
+        GameObject s = selected[(int)pi];
+        if (!s) s = starts[(int)pi];
+        CharacterSelectButtons b = s.GetComponent<CharacterSelectButtons>();
+        if (!b) return;
+        if (!b.interactable) s = starts[(int)pi];
+
+        Control<CAxis> cHor = new Control<CAxis>(CAxis.LX, pi);
+        Control<CAxis> cVer = new Control<CAxis>(CAxis.LY, pi);
+
+        GamePadState pad = GamePad.GetState(pi);
+    
+        float horizontal = GamePad.GetAxis(cHor);
+        float vertical = GamePad.GetAxis(cVer);
+
+        
+
+        if (!ready[(int)pi])
+        {
+            //HORIZONTAL
+            if (horizontal != 0)
+            {
+                if (!Locked(cHor))
+                {
+                    if (horizontal < 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().left;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cHor);
+                    }
+                    else if (horizontal > 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().right;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cHor);
+                    }
+                }
+            }
+            else
+            {
+                Unlock(cHor);
+            }
+
+            //VERTICAL
+            if (vertical != 0)
+            {
+                if (!Locked(cVer))
+                {
+                    if (vertical < 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().down;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cVer);
+                    }
+                    else if (vertical > 0)
+                    {
+                        GameObject swap = s.GetComponent<CharacterSelectButtons>().up;
+                        if (swap && swap.GetComponent<CharacterSelectButtons>() && swap.GetComponent<CharacterSelectButtons>().interactable) s = swap;
+                        Lock(cVer);
+                    }
+                }
+            }
+            else
+            {
+                Unlock(cVer);
+            }
+
+
+
+            //Submit Selection
+
+            if (selected[(int)pi] != s)
+            {
+                selected[(int)pi].GetComponent<CharacterSelectButtons>().Deselect();
+                s.GetComponent<CharacterSelectButtons>().Select();
+            }
+            selected[(int)pi] = s;
+
+
+            //Check For Continue
+            if (pad.Released(CButton.A))
+            {
+                    if (pi == PlayerIndex.One)
+                    {
+                        data.player1Char = selected[(int)pi].GetComponent<CharacterSelectButtons>().character;
+                        ready[(int)pi] = true;
+                    }
+                    else if (pi == PlayerIndex.Two)
+                    {
+                        data.player2Char = selected[(int)pi].GetComponent<CharacterSelectButtons>().character;
+                        ready[(int)pi] = true;
+                    }
+                }
+
+        }
+        else
+        {
+            if (pad.Released(CButton.B))
+            {
+                ready[(int)pi] = false;
+            }
+        }
+
+
+       
+    }
+
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("MapSelect");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public bool Locked(Control<CAxis> control)
+    {
+        return locker.HasLock(control);
+    }
+
+    public void Unlock(Control<CAxis> control)
+    {
+        locker.Unlock(control);
+    }
+
+    public void Lock(Control<CAxis> control)
+    {
+        locker.Lock(control);
+    }
+
+    public bool Locked(Control<CButton> control)
+    {
+        return locker.HasLock(control);
+    }
+
+    public void Unlock(Control<CButton> control)
+    {
+        locker.Unlock(control);
+    }
+
+    public void Lock(Control<CButton> control)
+    {
+        locker.Lock(control);
+    }
+
+
+    public GameObject[] GetSelected()
+    {
+        return selected;
+    }
+
+    
+}
